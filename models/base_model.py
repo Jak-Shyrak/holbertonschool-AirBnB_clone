9d54d2346e628d5136f81d1a770f751db78d6ac5
lchenevi <leo.cheneviere@gmail.com>
2024-03-02 16:55:45 +0100

    pushing the tests files and adding commented version of the base_model file

--- models/base_model.py
+++ models/base_model.py
@@ -1,35 +1,43 @@
 #!/usr/bin/python3
+"""
+BaseModel class module
+
+
+"""
+
 import uuid
 from datetime import datetime
 
 class BaseModel:
+    """
+    BaseModel class
+    """
     def __init__(self):
+        """
+        Initialize instance attributes
+        """
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        Define the string representation of the object
+        """
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """
+        Update the 'updated_at' attribute with the current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Convert the object to a dictionary representation
+        """
         obj_dict = self.__dict__.copy()
         obj_dict['__class__'] = self.__class__.__name__
         obj_dict['created_at'] = self.created_at.isoformat()
         obj_dict['updated_at'] = self.updated_at.isoformat()
         return obj_dict
-
-if __name__ == "__main__":
-    my_model = BaseModel()
-    my_model.name = "My First Model"
-    my_model.my_number = 89
-    print(my_model)
-    my_model.save()
-    print(my_model)
-    my_model_json = my_model.to_dict()
-    print(my_model_json)
-    print("JSON of my_model:")
-    for key in my_model_json.keys():
-        print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
