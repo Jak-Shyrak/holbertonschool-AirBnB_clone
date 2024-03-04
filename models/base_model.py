38ffdf122de8b44c30da5574f4a4990184837a6c
JakShyrak <bourhlef.yacine53@gmail.com>
2024-03-04 17:48:10 +0100

    Update base model

--- models/base_model.py
+++ models/base_model.py
@@ -11,20 +11,28 @@ from datetime import datetime
 class BaseModel:
     """
     BaseModel class
-    """
-    def __init__(self):
+    """        
+    def __init__(self, *args, **kwargs):
         """
         Initialize instance attributes
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+        if kwargs:
+            for key, value in kwargs.items():
+                if key != '__class__':
+                    if key in ['created_at', 'updated_at']:
+                        value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
+                    setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """
         Define the string representation of the object
         """
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
+        return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
+    
 
     def save(self):
         """
