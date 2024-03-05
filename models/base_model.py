2b0747445d86522e7fcd265140ef2d99d8405d13
lchenevi <leo.cheneviere@gmail.com>
2024-03-05 17:15:36 +0100

    modifying the base model file (adding storage)

--- models/base_model.py
+++ models/base_model.py
@@ -7,11 +7,12 @@ BaseModel class module
 
 import uuid
 from datetime import datetime
+from models import storage
 
 class BaseModel:
     """
     BaseModel class
-    """        
+    """
     def __init__(self, *args, **kwargs):
         """
         Initialize instance attributes
@@ -26,19 +27,21 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """
         Define the string representation of the object
         """
         return ("[{}] ({}) {}".format(self.__class__.__name__,self.id, self.__dict__))
-    
+
 
     def save(self):
         """
         Update the 'updated_at' attribute with the current datetime
         """
         self.updated_at = datetime.now()
+        storage.save()
 
     def to_dict(self):
         """
