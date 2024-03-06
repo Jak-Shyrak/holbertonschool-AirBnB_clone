393d562d0c5decc39a2b1ee379533f68ebf0aef7
JakShyrak <bourhlef.yacine53@gmail.com>
2024-03-06 21:51:05 +0100

    Update file strorage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -8,32 +8,38 @@ FileStorage class module
 import json
 from models.base_model import BaseModel
 
+
 class FileStorage:
     __file_path = "file.json"
     __objects = {}
 
     def all(self):
-        return FileStorage.__objects
+        """
+        Returns the __objects dictionnary
+        """
+        return self.__objects
 
     def new(self, obj):
-        key = "{}.{}".format(obj.__class__.__name__, obj.id)
-        FileStorage.__objects[key] = obj
+        """
+        Sets obj in __objects
+        """
+        key = f"{obj.__class__.__name__}.{obj.id}"
+        self.__objects[key] = obj
 
     def save(self):
-        serialized_objects = {}
-        for key, obj in FileStorage.__objects.items():
-            serialized_objects[key] = obj.to_dict()
-        with open(FileStorage.__file_path, 'w') as file:
-            json.dump(serialized_objects, file)
+        """
+        Serializes __objects to the JSON file
+        """
+        with open(self.__file_path, "w", encoding="utf-8") as f:
+            json.dump({k: v.to_dict() for k, v in self.__objects.items()}, f)
 
     def reload(self):
         try:
-            with open(FileStorage.__file_path, 'r') as file:
-                data = json.load(file)
+            with open(self.__file_path, "r", encoding="utf-8") as f:
+                data = json.load(f)
                 for key, value in data.items():
-                    class_name, obj_id = key.split(".")
-                    cls = eval(class_name)
-                    obj = cls(**value)
-                    FileStorage.__objects[key] = obj
+                    cls_name, obj_id = key.split('.')
+                    inst_cls = eval(cls_name)(**value)
+                    self.__objects[key] = inst_cls
         except FileNotFoundError:
             pass
