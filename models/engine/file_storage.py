1ac6be546ff1ad84c7d49f6b4d0068f2021ff9e0
JakShyrak <bourhlef.yacine53@gmail.com>
2024-03-06 22:20:38 +0100

    Update file_storage to Deserializes all JSON files to object

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,7 +33,13 @@ class FileStorage:
 
     def reload(self):
         """Deserializes the JSON file to __objects"""
+        from models.base_model import BaseModel
         from models.user import User
+        from models.state import State
+        from models.city import City
+        from models.review import Review
+        from models.amenity import Amenity
+        from models.place import Place
         try:
             with open(self.__file_path, "r", encoding="utf-8") as f:
                 data = json.load(f)
